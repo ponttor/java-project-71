5bd45fd53f109f6cb8be0ee44e6a4a54f90cea16
Alexander Illarionov <ruzinovrun@gmail.com>
2026-02-10 15:23:53 +0100

    add sonarqube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("application")
     id("checkstyle")
+    id("jacoco")
+    id("org.sonarqube") version "7.1.0.6387"
     id("com.github.ben-manes.versions") version "0.53.0"
 }
 
@@ -11,12 +13,23 @@ repositories {
     mavenCentral()
 }
 
+jacoco {
+    toolVersion = "0.8.12"
+}
+
 dependencies {
     implementation("tools.jackson.core:jackson-databind:3.0.0")
     implementation("info.picocli:picocli:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "ponttor_java-project-71")
+        property("sonar.organization", "ponttor")
+    }
 }
 
 tasks.test {
@@ -30,6 +43,19 @@ checkstyle {
 
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
 }
 
 application {
